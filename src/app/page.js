eefb92217fbf824cdc1bf380196387158db5748d
Anthony <anthony.s.quispe@gmail.com>
2024-03-03 23:02:53 -0500

    Finish Hero Section Desktop

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,43 @@
+import "./Landingpage.scss";
+import Logo from "../assets/logo/Spicynuggs-Logo.svg";
+import HeroImage from "../../public/assets/HeroStickerImage.png";
+import HeroStickers from "../../public/assets/stickers/Hero-Stickers.svg";
+
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <>
-      <div></div>
-    </>
+    <main className="landing-page">
+      <section className="landing-page__hero">
+        <div className="landing-page__hero-left-container">
+          <Image
+            className="landing-page__hero-logo"
+            src={Logo}
+            alt="Hero logo"
+          />
+          <div className="landing-page__hero-stickers-container">
+            <Image
+              className="landing-page__hero-stickers"
+              src={HeroStickers}
+              alt="Hero Stickers"
+            />
+          </div>
+          <div className="landing-page__hero-text-container">
+            <h1 className="landing-page__hero-title">
+              Browse Our Latest Products Now!
+            </h1>
+            <Link href="/shop" className="landing-page__hero-button">
+              SHOP NOW
+            </Link>
+          </div>
+        </div>
+        <Image
+          className="landing-page__hero-image"
+          src={HeroImage}
+          alt="Holding a Halloween Sticker"
+        />
+      </section>
+    </main>
   );
 }
