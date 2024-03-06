3b3b2e22525b0bda89c2da3091d7f4599ab3071f
Anthony <anthony.s.quispe@gmail.com>
2024-03-06 11:56:06 -0500

    Finished styling info page

---
+++ src/app/info/page.jsx
@@ -0,0 +1,64 @@
+import "./info.scss";
+
+function Info() {
+  return (
+    <div className="info">
+      <div className="info__container">
+        <h2 className="info__shipping-title"> Shipping Info</h2>
+        <p className="info__shipping-description">
+          Shipping Information for SpicyNuggs Stickers: Domestic (within United
+          State): Standard Shipping: 3-7 business days - $X.XX Express Shipping:
+          1-3 business days - $X.XX International: Standard International: 7-14
+          business days - $X.XX and up (depending on location) Express
+          International: 3-5 business days - $X.XX and up (depending on
+          location) Please note that shipping times and costs may vary based on
+          the destination and chosen shipping method. SpicyNuggs aims to process
+          orders within 1-2 business days after payment confirmation. Customers
+          will receive a tracking number via email once the order is dispatched
+          to track the shipment's progress. For specific and accurate shipping
+          details, customers are encouraged to visit SpicyNuggs's website or
+          contact customer support for more precise and updated information.
+        </p>
+      </div>
+      <div className="info__container">
+        <h2 className="info__terms-title"> Terms of Service</h2>
+        <p className="info__terms-description">
+          Welcome to SpicyNuggs! These Terms of Service outline the rules and
+          regulations for the use of our website. By accessing this website, we
+          assume you accept these terms and conditions. Do not continue to use
+          SpicyNuggs if you do not agree to take all of the terms and conditions
+          stated on this page. The following terminology applies to these Terms
+          of Service, Privacy Statement, and Disclaimer Notice and all
+          agreements: "Client," "You," and "Your" refers to you, the person
+          accessing this website and accepting the company’s terms and
+          conditions. "The Company," "Ourselves," "We," "Our," and "Us" refer to
+          our Company. "Party," "Parties," or "Us" refer to both the Client and
+          ourselves, or either the Client or ourselves. Cookies We employ the
+          use of cookies. By accessing SpicyNuggs, you agreed to use cookies in
+          agreement with the SpicyNuggs' Privacy Policy. License Unless
+          otherwise stated, SpicyNuggs and/or its licensors own the intellectual
+          property rights for all material on SpicyNuggs. All intellectual
+          property rights are reserved. You may access this from SpicyNuggs for
+          your personal use subjected to restrictions set in these terms and
+          conditions. You must not: Republish material from SpicyNuggs Sell,
+          rent, or sub-license material from SpicyNuggs Reproduce, duplicate, or
+          copy material from SpicyNuggs Redistribute content from SpicyNuggs
+          **Parts of this website offer an opportunity for users to post and
+          exchange opinions and information. SpicyNuggs does not filter, edit,
+          publish, or review Comments prior to their presence on the website.
+          Comments do not reflect the views and opinions of SpicyNuggs, its
+          agents, and/or affiliates. Comments reflect the views and opinions of
+          the person who posts their views and opinions. To the extent permitted
+          by applicable laws, SpicyNuggs shall not be liable for the Comments or
+          for any liability, damages, or expenses caused and/or suffered as a
+          result of any use of and/or posting of and/or appearance of the
+          Comments on this website. We reserve the right to monitor all Comments
+          and to remove any Comments which can be considered inappropriate,
+          offensive, or causes a breach of these Terms and Conditions.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Info;
